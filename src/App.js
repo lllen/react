e18b139ce5aeca2f5cf78fa7dc2289b5c0ce8169
Olena <olena@qlicks.nl>
2019-10-03 18:49:53 +0300

    Added plots plugin

--- src/App.js
+++ src/App.js
@@ -1,51 +1,14 @@
+/* eslint-disable import/first */
 import React from 'react';
 import logo from './logo.svg';
-import jQuery from 'jquery';
-import flot from 'flot';
 import './App.css';
+import PieChart from './plots/Pie Chart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-        <Flot />
-      </div>
-    </div>
-  );
-}
-
-class Flot extends React.Component {
-  constructor(props) {
-    super(props);
-    this.placeholder = 'Test';
-    this.options = {
-      series: {
-          lines: { show: true },
-          points: { show: true }
-      }
-  };
-  };
-
-  componentDidMount () {
-    jQuery.plot(jQuery("#fplot"), [[0, 5], [1, 10], [2, 15], [3, 20]]);
-  };
-
-  render () {
-    return <div id="#fplot"></div>;
+class App extends React.Component {
+    render() {
+      return (
+          <PieChart />
+    );
   }
 }
 
